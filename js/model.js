73cbe86330feb649c7353a8aaa939650fc1cad01
Nemrow <jnemrow@shopittome.com>
2014-08-27 18:15:41 -0700

    Make snake case for ruby

--- js/model.js
+++ js/model.js
@@ -6,7 +6,7 @@ Model = {
       {
         selector: "article#pagecontainer section.body table#container tbody tr td#center table#main tbody tr td:nth-of-type(2) div.col:nth-of-type(1) div.cats ul#hhh0 li:nth-of-type(5) a.swp span.txt",
         content: "This is the new name",
-        contentType: "text"
+        content_type: "text"
       }
     ]
   }
