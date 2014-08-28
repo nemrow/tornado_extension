c815bcdc27ba316e46ab848593ff7177f19596ec
Nemrow <jnemrow@shopittome.com>
2014-08-28 14:08:49 -0700

    WIP: Need to put site back to normal here

--- js/init.js
+++ js/init.js
@@ -4,8 +4,10 @@ chrome.runtime.onMessage.addListener(function(request) {
     Controller.deactivateLinks();
     Controller.displayFuckedUpContent();
     Controller.activateClickables();
-  } else {
-
   } else if (Model.editableMode == "view mode") {
+    // put the site back to normal
+  } else if (Model.editableMode == "editing mode") {
+    // ask if the user wants to quit before saving new edit
+    // then put back to normal
   }
 });
