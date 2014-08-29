f52727f8a5d4c5c767e1a47be064d7e8668e2e58
Nemrow <jnemrow@shopittome.com>
2014-08-29 14:53:00 -0700

    Allow toggling

--- js/init.js
+++ js/init.js
@@ -1,14 +1,37 @@
-chrome.runtime.onMessage.addListener(function(request) {
-  if (Model.editableMode == "not active") {
-    View.loadTemplates();
-    Controller.deactivateLinks();
-    Controller.displayFuckedUpContent();
-    Controller.activateChangeSubmit();
-    Controller.activateClickables();
-  } else if (Model.editableMode == "view mode") {
-    // put the site back to normal
-  } else if (Model.editableMode == "editing mode") {
-    // ask if the user wants to quit before saving new edit
-    // then put back to normal
+theTornado = {
+  triggerMode: function () {
+    if (Model.editableMode == "edit") {
+      View.loadTemplates();
+      Controller.deactivateLinks();
+      Controller.displayFuckedUpContent();
+      Controller.activateChangeSubmit();
+      Controller.activateClickables();
+      View.displayBorders();
+    } else if (Model.editableMode == "view") {
+      Controller.deactivateClickables();
+      View.removeEditableViews();
+      Controller.activateLinks();
+      Controller.displayFuckedUpContent();
+    } else if (Model.editableMode == "off") {
+      Controller.deactivateClickables();
+      View.removeEditableViews();
+      Controller.activateLinks();
+    }
   }
+};
+
+// To set the current mode
+chrome.runtime.sendMessage({requestType: "getMode"}, function(response) {
+  console.log("Mode response from call: " + response.mode);
+  Model.editableMode = response.mode;
+  theTornado.triggerMode();
 });
+
+// When the user click a new mode in the popup
+chrome.runtime.onMessage.addListener(function(request) {
+  if (request.mode) {
+    Model.editableMode = request.mode;
+    theTornado.triggerMode();
+  };
+});
+
--- js/view.js
+++ js/view.js
@@ -13,17 +13,29 @@ View = {
     $('body').append(html);
   },
 
+  removeEditableViews: function () {
+    $('.fuck-it-up-borders').remove();
+    $('.fuck-it-up-form').remove();
+  },
+
   displayEdittingForm: function (event) {
     Controller.deactivateClickables();
     var currentTarget = $(event.target);
     var currentCssPath = cssPath.getCssPath(currentTarget);
-    if (currentTarget.prop('tagName').toLowerCase() == "img") {
+    var currentPropName = currentTarget.prop('tagName').toLowerCase();
+    if (currentPropName == "img") {
       var html = Templates.fuckItUpImageForm(currentCssPath, document.URL)
       $('body').append(html);
-    } else {
+    } else if (View.textEditable(currentTarget)) {
       var html = Templates.fuckItUpTextForm(currentCssPath, document.URL)
       $('body').append(html);
+    } else {
+      alert("Can't edit this yet. Shit. Oh well, this app is pointless anyway.");
     }
+  },
+
+  textEditable: function (node) {
+    return (node.children().length == 0 && node.text().length > 0)
   },
 
   removeEdittingForm: function (event) {
